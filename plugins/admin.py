3dc88fb9e78051e4409568ab2600309d8dca2f37
wywxz <53309090+wywxz@users.noreply.github.com>
2022-08-12 21:08:52 -0400

    update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -1,14 +1,12 @@
 
-# (c) @AbirHasan2005 | Modifieded By : @DC4_WARRIOR
-
 from pyrogram import Client as Clinton
 from pyrogram import filters
 from config import Config
 from database.access import clinton
-from plugins.buttons import *
+
 @Clinton.on_message(filters.private & filters.command('total'))
-async def sts(c, m):
-    if m.from_user.id != Config.OWNER_ID:
+async def stats(bot, update):
+    if update.from_user.id != Config.OWNER_ID:
         return 
     total_users = await clinton.total_users_count()
-    await m.reply_text(text=f"<b>Total users:</b> {total_users}", quote=True)
+    await update.reply_text(text=f"<b>Total users:</b> {total_users}", quote=True)
