99b3aee197eba56a21df99f8a1cfc2ac78dc9696
wywxz <53309090+wywxz@users.noreply.github.com>
2022-08-13 22:07:55 -0400

    update dl_button.py

--- plugins/dl_button.py
+++ plugins/dl_button.py
@@ -2,7 +2,7 @@
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-import asyncio, aiohttp, os, time
+import asyncio, aiohttp, os, time, math
 from datetime import datetime
 from config import Config
 from translation import Translation
