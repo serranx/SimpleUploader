7007bc064a918728e4d6a6dfa98919c3a66ca2ac
wywxz <53309090+wywxz@users.noreply.github.com>
2022-08-18 19:53:41 -0400

    Rename streamlare.py to zplayer.py

---
+++ plugins/zplayer.py
@@ -0,0 +1,31 @@
+
+import requests
+import asyncio
+import aiohttp
+import os
+import time
+import math
+from datetime import datetime
+from bs4 import BeautifulSoup
+from config import Config
+from translation import Translation
+from plugins.custom_thumbnail import *
+from helper_funcs.display_progress import progress_for_pyrogram, humanbytes, TimeFormatter
+
+async def get(url):
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Max-Age': '3600',
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+        }
+    req = requests.get(url, headers)
+    time.sleep(10)
+    print(req.content)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    #dl_url = soup.find_all("https://larecontent.com/download").get("href")
+    dl_url = soup.a
+    print(dl_url)
+    filename = "idjfbxno"
+    return "{}|{}".format(dl_url, filename)
