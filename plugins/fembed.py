abe24a4a11a870ab3b256986dee5dee690bf8903
wywxz <53309090+wywxz@users.noreply.github.com>
2022-08-19 14:49:25 -0400

    update fembed.py

--- plugins/fembed.py
+++ plugins/fembed.py
@@ -201,6 +201,6 @@ async def download_coroutine(info_msg, session, url, file_name, start):
                             display_message = current_message
                             time.sleep(0.5)
                     except Exception as e:
-                        logger.info(str(e))
+                        #logger.info(str(e))
                         pass
         return await response.release()
