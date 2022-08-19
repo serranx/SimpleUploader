0aac24e467959d854152acc9d59be42979cee000
wywxz <53309090+wywxz@users.noreply.github.com>
2022-08-18 23:30:20 -0400

    Rename zplayer.py to module00.py

---
+++ plugins/module00.py
@@ -0,0 +1,28 @@
+
+import requests
+import asyncio
+import aiohttp
+import os
+import time
+import math
+from datetime import datetime
+from bs4 import BeautifulSoup
+from config import Config
+from translation import Translation
+from plugins.custom_thumbnail import *
+from helper_funcs.display_progress import progress_for_pyrogram, humanbytes, TimeFormatter
+
+async def get(url):
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content, "html.parser")
+    try:
+        dl_url = soup.find("a", class_="uk-button uk-button-danger").get("href")
+    except:
+        try:
+            error_msg = soup.find("p", class_="uk-text-danger uk-text-center").get_text()
+            print(error_msg)
+        except Exception as e:
+            print(req.content)
+            print("Unknown error.\n"+str(e))
+    filename = "test.mp4"
+    return dl_url, filename
