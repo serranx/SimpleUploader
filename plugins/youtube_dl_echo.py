554f1e87c8020a172bcbc30195624e89d6c45cfc
wywxz <53309090+wywxz@users.noreply.github.com>
2022-08-20 18:59:10 -0400

    update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -2,27 +2,39 @@
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-import os, re, time, asyncio, json, random, string, requests
+
+import sys, requests, urllib.parse, filetype, os, time, shutil, tldextract, asyncio, json, math
+
 from config import Config
 from database.adduser import AddUser
 from translation import Translation
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from pyrogram import filters
 from pyrogram import Client as Clinton
+from database.access import clinton
 from helper_funcs.display_progress import humanbytes
+from helper_funcs.help_uploadbot import DownLoadFile
+from helper_funcs.display_progress import progress_for_pyrogram, humanbytes, TimeFormatter
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import UserNotParticipant
 
-@Clinton.on_message(filters.private & filters.regex(pattern=".*http.*"))
-async def echo(bot, message):
-    await AddUser(bot, message)
-    info_msg = await message.reply_text(
-        "<b>Processing...⏳</b>", 
-        quote=True
-    )
+@Clinton.on_message(filters.private & filters.regex(pattern=".*http.*") & ~filters.regex(pattern="\.mediafire\.com") & ~filters.regex(pattern="fembed\.com|fembed-hd\.com|femax20\.com|vanfem\.com|suzihaza\.com|owodeuwu\.xyz"))
+async def echo(bot, update):
+    await AddUser(bot, update)
+    imog = await update.reply_text("<b>Processing... ⏳</b>", reply_to_message_id=update.message_id)
+    if os.path.exists(Config.DOWNLOAD_LOCATION + "/" + str(update.chat.id) + ".json"):
+        await bot.edit_message_text(
+            text=Translation.WAIT_PROCESS_FINISH,
+            chat_id=update.chat.id,
+            message_id=imog.message_id
+        )
+        return False
     youtube_dl_username = None
     youtube_dl_password = None
     file_name = None
-    url = message.text
+    url = update.text
     if " * " in url:
         url_parts = url.split(" * ")
         if len(url_parts) == 2:
@@ -34,7 +46,7 @@ async def echo(bot, message):
             youtube_dl_username = url_parts[2]
             youtube_dl_password = url_parts[3]
         else:
-            for entity in message.entities:
+            for entity in update.entities:
                 if entity.type == "text_link":
                     url = entity.url
                 elif entity.type == "url":
@@ -51,7 +63,7 @@ async def echo(bot, message):
         if youtube_dl_password is not None:
             youtube_dl_password = youtube_dl_password.strip()
     else:
-        for entity in message.entities:
+        for entity in update.entities:
             if entity.type == "text_link":
                 url = entity.url
             elif entity.type == "url":
@@ -90,8 +102,10 @@ async def echo(bot, message):
     )
     # Wait for the subprocess to finish
     stdout, stderr = await process.communicate()
-    await info_msg.edit_text(
-        "<b>Processing...⌛</b>"
+    await bot.edit_message_text(
+        text="<b>Processing... ⌛</b>",
+        chat_id=update.chat.id,
+        message_id=imog.message_id
     )
     time.sleep(0.5)
     e_response = stderr.decode().strip()
@@ -99,25 +113,25 @@ async def echo(bot, message):
     # https://github.com/rg3/youtube-dl/issues/2630#issuecomment-38635239
     if e_response and "nonnumeric port" not in e_response:
         # logger.warn("Status : FAIL", exc.returncode, exc.output)
-        #error_message = e_response.replace("please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; see  https://yt-dl.org/update  on how to update. Be sure to call youtube-dl with the --verbose flag and include its complete output.", "")
-        error_message = e_response.replace("please report this issue on  https://github.com/yt-dlp/yt-dlp/issues?q= , filling out the appropriate issue template. Confirm you are on the latest version using  yt-dlp -U", "")
+        error_message = e_response.replace("please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; see  https://yt-dl.org/update  on how to update. Be sure to call youtube-dl with the --verbose flag and include its complete output.", "")
         if "This video is only available for registered users." in error_message:
             error_message += Translation.SET_CUSTOM_USERNAME_PASSWORD
-        await info_msg.edit(
+        await imog.delete(True)
+        await bot.send_message(
+            chat_id=update.chat.id,
             text=Translation.NO_VOID_FORMAT_FOUND.format(str(error_message)),
+            reply_to_message_id=update.message_id,
+            parse_mode="html",
             disable_web_page_preview=True
         )
-        return
+        return False
     if t_response:
         x_reponse = t_response
         if "\n" in x_reponse:
             x_reponse, _ = x_reponse.split("\n")
         response_json = json.loads(x_reponse)
-        json_name = "".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
-        tmp_directory_for_each_user = Config.DOWNLOAD_LOCATION + str(message.from_user.id)
-        if not os.path.isdir(tmp_directory_for_each_user):
-            os.makedirs(tmp_directory_for_each_user)
-        save_ytdl_json_path = tmp_directory_for_each_user + "/" + json_name + ".json"
+        save_ytdl_json_path = Config.DOWNLOAD_LOCATION + \
+            "/" + str(update.from_user.id) + ".json"
         with open(save_ytdl_json_path, "w", encoding="utf8") as outfile:
             json.dump(response_json, outfile, ensure_ascii=False)
         inline_keyboard = []
@@ -128,157 +142,101 @@ async def echo(bot, message):
             for formats in response_json["formats"]:
                 format_id = formats.get("format_id")
                 format_string = formats.get("format_note")
-                format_ext = formats.get("ext")
-                approx_file_size = ""
                 if format_string is None:
                     format_string = formats.get("format")
-                if "x-matroska" in format_string:
-                    format_string = "mkv"
-                if "unknown" in format_string:
-                    format_string = format_ext
+                format_ext = formats.get("ext")
+                approx_file_size = ""
                 if "filesize" in formats:
                     approx_file_size = humanbytes(formats["filesize"])
-                cb_string_video = "{}|{}|{}|{}".format(
-                    "video", format_id, format_ext, json_name)
-                cb_string_file = "{}|{}|{}|{}".format(
-                    "file", format_id, format_ext, json_name)
+                cb_string_video = "{}|{}|{}".format(
+                    "video", format_id, format_ext)
+                cb_string_file = "{}|{}|{}".format(
+                    "file", format_id, format_ext)
                 if format_string is not None and not "audio only" in format_string:
-                    if re.match("(http(s)?):\/\/(www\.)?youtu(be)?\.(com|be)", url) and re.match("storyboard|ultralow|low|medium", format_string):
-                        continue
-                    if format_ext in Config.VIDEO_FORMATS:
-                    	  ikeyboard = [
-                            InlineKeyboardButton(
-                                "🎥 video " + format_string + " " + approx_file_size,
-                                callback_data=(cb_string_video).encode("UTF-8")
-                            ),
-                            InlineKeyboardButton(
-                                "📄 file " + format_ext + " " + approx_file_size,
-                                callback_data=(cb_string_file).encode("UTF-8")
-                            )
-                        ]
-                    elif format_ext in Config.AUDIO_FORMATS:
-                    	  ikeyboard = [
-                            InlineKeyboardButton(
-                                "🎧 audio " + format_ext + " " + approx_file_size,
-                                callback_data=(cb_string_video).encode("UTF-8")
-                            ),
-                            InlineKeyboardButton(
-                                "📄 file " + format_ext + " " + approx_file_size,
-                                callback_data=(cb_string_file).encode("UTF-8")
-                            )
-                        ]
-                    else:
-                  	    ikeyboard = [
-                            InlineKeyboardButton(
-                                "📄 file " + format_ext + " " + approx_file_size,
-                                callback_data=(cb_string_file).encode("UTF-8")
-                            )
-                        ]
+                    ikeyboard = [
+                        InlineKeyboardButton(
+                            "🎥 video " + format_string.split("-")[0] + " " + approx_file_size,
+                            callback_data=(cb_string_video).encode("UTF-8")
+                        ),
+                        InlineKeyboardButton(
+                            "📄 file " + format_ext + " " + approx_file_size,
+                            callback_data=(cb_string_file).encode("UTF-8")
+                        )
+                    ]
                 else:
                     # special weird case :\
-                    if format_ext in Config.VIDEO_FORMATS:
-                    	  ikeyboard = [
-                            InlineKeyboardButton(
-                                "🎥 video " + format_ext,
-                                callback_data=(cb_string_video).encode("UTF-8")
-                            ),
-                            InlineKeyboardButton(
-                                "📄 file " + format_ext,
-                                callback_data=(cb_string_file).encode("UTF-8")
-                            )
-                        ]
-                    elif format_ext in Config.AUDIO_FORMATS:
-                    	  ikeyboard = [
-                            InlineKeyboardButton(
-                                "🎧 audio " + format_ext + " " + approx_file_size,
-                                callback_data=(cb_string_video).encode("UTF-8")
-                            ),
-                            InlineKeyboardButton(
-                                "📄 file " + format_ext + " " + approx_file_size,
-                                callback_data=(cb_string_file).encode("UTF-8")
-                            )
-                        ]
-                    else:
-                        ikeyboard = [
-                            InlineKeyboardButton(
-                                "📄 file " + format_ext + " " + approx_file_size,
-                                callback_data=(cb_string_file).encode("UTF-8")
-                            )
-                        ]
+                    ikeyboard = [
+                        InlineKeyboardButton(
+                            "🎥 video - " + format_ext,
+                            callback_data=(cb_string_video).encode("UTF-8")
+                        ),
+                        InlineKeyboardButton(
+                            "📄 file - " + format_ext,
+                            callback_data=(cb_string_file).encode("UTF-8")
+                        )
+                    ]
                 inline_keyboard.append(ikeyboard)
             if duration is not None:
-                cb_string_64 = "{}|{}|{}|{}".format("audio", "64k", "mp3", json_name)
-                cb_string_128 = "{}|{}|{}|{}".format("audio", "128k", "mp3", json_name)
-                cb_string = "{}|{}|{}|{}".format("audio", "320k", "mp3", json_name)
+                cb_string_64 = "{}|{}|{}".format("audio", "64k", "mp3")
+                cb_string_128 = "{}|{}|{}".format("audio", "128k", "mp3")
+                cb_string = "{}|{}|{}".format("audio", "320k", "mp3")
                 inline_keyboard.append([
                     InlineKeyboardButton(
-                        "🎧 MP3 (64 kbps)", callback_data=cb_string_64.encode("UTF-8")),
+                        "🎧 MP3 " + "(" + "64 kbps" + ")", callback_data=cb_string_64.encode("UTF-8")),
                     InlineKeyboardButton(
-                        "🎧 MP3 (128 kbps)", callback_data=cb_string_128.encode("UTF-8"))
+                        "🎧 MP3 " + "(" + "128 kbps" + ")", callback_data=cb_string_128.encode("UTF-8"))
                 ])
                 inline_keyboard.append([
                     InlineKeyboardButton(
-                        "🎧 MP3 (320 kbps)", callback_data=cb_string.encode("UTF-8"))
+                        "🎧 MP3 " + "(" + "320 kbps" + ")", callback_data=cb_string.encode("UTF-8"))
                 ])
         else:
-            total_length = ""
-            try:
-                total_length = requests.get(url, stream=True).headers["Content-Length"]
-            except Exception as e:
-                await info_msg.edit(
-                    text=Translation.NO_VOID_FORMAT_FOUND.format(str(e)),
-                    disable_web_page_preview=True
-                )
-                return
             format_id = response_json["format_id"]
             format_ext = response_json["ext"]
-            cb_string_file = "{}={}={}={}".format(
-                "file", format_id, format_ext, json_name)
-            cb_string_video = "{}={}={}={}".format(
-                "video", format_id, format_ext, json_name)
-            if format_ext in Config.VIDEO_FORMATS:
-                inline_keyboard.append([
-                    InlineKeyboardButton(
-                        #"🎥 video - " + format_ext + " " + humanbytes(int(total_length)),
-                        "🎥 video - " + format_ext + " " + int(total_length),
-                        callback_data=(cb_string_video).encode("UTF-8")
-                    ),
-                    InlineKeyboardButton(
-                        #"📃 file - " + format_ext + " " + humanbytes(int(total_length)),
-                        "📃 file - " + format_ext + " " + int(total_length),
-                        callback_data=(cb_string_file).encode("UTF-8")
-                    )
-                ])
-            elif format_ext in Config.AUDIO_FORMATS:
-            	  ikeyboard = [
-                    InlineKeyboardButton(
-                        "🎧 audio - " + format_ext + " " + approx_file_size,
-                        callback_data=(cb_string_video).encode("UTF-8")
-                    ),
-                    InlineKeyboardButton(
-                        "📄 file - " + format_ext + " " + approx_file_size,
-                        callback_data=(cb_string_file).encode("UTF-8")
-                    )
-                ]
-            else:
-                inline_keyboard.append([
-                    InlineKeyboardButton(
-                        "📃 file - " + format_ext + " " + humanbytes(int(total_length)),
-                        callback_data=(cb_string_file).encode("UTF-8")
-                    )
-                ])
+            cb_string_file = "{}|{}|{}".format(
+                "file", format_id, format_ext)
+            cb_string_video = "{}|{}|{}".format(
+                "video", format_id, format_ext)
+            inline_keyboard.append([
+                InlineKeyboardButton(
+                    "🎥 video - " + format_ext,
+                    callback_data=(cb_string_video).encode("UTF-8")
+                ),
+                InlineKeyboardButton(
+                    "📄 file - " + format_ext,
+                    callback_data=(cb_string_file).encode("UTF-8")
+                )
+            ])
+            cb_string_file = "{}={}={}".format(
+                "file", format_id, format_ext)
+            cb_string_video = "{}={}={}".format(
+                "video", format_id, format_ext)
+            inline_keyboard.append([
+                InlineKeyboardButton(
+                    "🎥 video - " + format_ext,
+                    callback_data=(cb_string_video).encode("UTF-8")
+                ),
+                InlineKeyboardButton(
+                    "📃 file - " + format_ext,
+                    callback_data=(cb_string_file).encode("UTF-8")
+                )
+            ])
         reply_markup = InlineKeyboardMarkup(inline_keyboard)
-        await info_msg.edit(
+        await imog.delete(True)
+        await bot.send_message(
+            chat_id=update.chat.id,
             text=Translation.FORMAT_SELECTION,
-            reply_markup=reply_markup
+            reply_markup=reply_markup,
+            parse_mode="html",
+            reply_to_message_id=update.message_id
         )
     else:
         # fallback for nonnumeric port a.k.a seedbox.io
         inline_keyboard = []
-        cb_string_file = "{}={}={}={}".format(
-            "file", "LFO", "NONE", json_name)
-        cb_string_video = "{}={}={}={}".format(
-            "video", "LFO", "NONE", json_name)
+        cb_string_file = "{}={}={}".format(
+            "file", "LFO", "NONE")
+        cb_string_video = "{}={}={}".format(
+            "video", "OFL", "ENON")
         inline_keyboard.append([
             InlineKeyboardButton(
                 "🎥 video",
@@ -290,7 +248,11 @@ async def echo(bot, message):
             )
         ])
         reply_markup = InlineKeyboardMarkup(inline_keyboard)
-        await info_msg.edit(
+        await imog.delete(True)
+        await bot.send_message(
+            chat_id=update.chat.id,
             text=Translation.FORMAT_SELECTION,
-            reply_markup=reply_markup
+            reply_markup=reply_markup,
+            parse_mode="html",
+            reply_to_message_id=update.message_id
         )
