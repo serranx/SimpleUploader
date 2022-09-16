5cef05037f3e37270b8d19c9a61099826bf291f6
wywxz <53309090+wywxz@users.noreply.github.com>
2022-09-16 12:18:41 -0400

    update mediafire.py

--- plugins/mediafire.py
+++ plugins/mediafire.py
@@ -15,7 +15,7 @@ async def get(url):
         'Access-Control-Max-Age': '3600',
         'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
     }
-    req = requests.get(url, stream=True)
+    req = requests.get(url, headers, stream=True)
     soup = BeautifulSoup(req.content, "html.parser")
     dl_url = soup.find("a", id="downloadButton").get("href")
     try:
