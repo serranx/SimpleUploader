df95497a9060f6122f49d3b3e2333b7e43d66be6
wywxz <53309090+wywxz@users.noreply.github.com>
2022-09-16 12:29:27 -0400

    update mediafire.py

--- plugins/mediafire.py
+++ plugins/mediafire.py
@@ -9,11 +9,11 @@ from helper_funcs.display_progress import progress_for_pyrogram, humanbytes, Tim
 
 async def get(url):
     headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Max-Age': '3600',
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Max-Age": "3600",
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0"
     }
     req = requests.get(url, stream=True)
     soup = BeautifulSoup(req.content, "html.parser")
