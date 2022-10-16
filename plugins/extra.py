6dac5e69b79df395449090195972e284347707fb
wywxz <53309090+wywxz@users.noreply.github.com>
2022-10-15 22:07:41 -0400

    update extra.py

--- plugins/extra.py
+++ plugins/extra.py
@@ -82,9 +82,10 @@ async def dl_1fichier(bot, message):
     try:
         raw = requests.get(url, stream=True)
         soup = BeautifulSoup(raw.text, "html.parser")
-        ext = soup.findAll("td", class_="normal")[1].get_text().split(".")[-1]
+        filename = soup.findAll("td", class_="normal")[1].get_text()
+        ext = filename.split(".")[-1]
         if custom_file_name is None:
-            custom_file_name = soup.findAll("td", class_="normal")[1].get_text()
+            custom_file_name = filename
         else:
             custom_file_name += "." + ext
         raw = requests.post(url, stream=True)
