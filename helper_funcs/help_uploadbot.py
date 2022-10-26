4ad41bd17cea747edfca88f1941811fb63a4ccc7
wywxz <53309090+wywxz@users.noreply.github.com>
2022-10-26 19:44:22 -0400

    update help_uploadbot.py

--- helper_funcs/help_uploadbot.py
+++ helper_funcs/help_uploadbot.py
@@ -1,21 +1,13 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
 
-# the logging things
 import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-
-import os
-import requests
+import os, requests
 
 def DetectFileSize(url):
     r = requests.get(url, allow_redirects=True, stream=True)
     total_size = int(r.headers.get("content-length", 0))
     return total_size
-
 
 def DownLoadFile(url, file_name, chunk_size, client, ud_type, message_id, chat_id):
     if os.path.exists(file_name):
