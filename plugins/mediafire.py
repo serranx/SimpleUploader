5e495581f2cc1f4729543292f3d0a01027c2bb65
wywxz <53309090+wywxz@users.noreply.github.com>
2022-11-24 10:12:16 -0400

    update mediafire.py

--- plugins/mediafire.py
+++ plugins/mediafire.py
@@ -181,6 +181,6 @@ async def download_coroutine(info_msg, session, url, file_name, start):
                             )
                             display_message = current_message
                     except Exception as e:
-                        #logger.info(str(e))
+                        logger.info(str(e) + " 🌝")
                         pass
         return await response.release()
