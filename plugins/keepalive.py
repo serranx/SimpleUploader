9df8c0b70e0dc681fa4e349fb4eeaaf940cd1986
wywxz <53309090+wywxz@users.noreply.github.com>
2022-11-28 14:12:50 -0400

    update keepalive.py

---
+++ plugins/keepalive.py
@@ -0,0 +1,21 @@
+
+import asyncio
+import logging
+import aiohttp
+import traceback
+from config import Config
+
+async def ping_server():
+    sleep_time = 600
+    while True:
+        await asyncio.sleep(sleep_time)
+        try:
+            async with aiohttp.ClientSession(
+                timeout=aiohttp.ClientTimeout(total=10)
+            ) as session:
+                async with session.get(Config.FQDN) as resp:
+                    logging.info("Pinged server with response: {}".format(resp.status))
+        except TimeoutError:
+            logging.warning("Couldn't connect to the site URL..!")
+        except Exception:
+            traceback.print_exc()
